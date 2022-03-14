cf2f42318bdcba188cb33b352d1bcc5249016d90
alapinilopez <alapinilopez@cifpfbmoll.eu>
2022-03-14 16:57:10 +0100

    fix: :bug: Elimina imports nunca utilizados
    Eliminar imports que no se utilizan

--- repository/create.py
+++ repository/create.py
@@ -1,7 +1,7 @@
 from audioop import add
 from email import parser
 import string
-from flask_restful import marshal_with, reqparse, Resource, Api, fields, g
+from flask_restful import marshal_with, reqparse, Resource, Api, fields
 import json
 
 parser = reqparse.RequestParser()
@@ -22,6 +22,9 @@ class Create(Resource):
     @marshal_with(resource_fields)
     def get(self):
         return (add_one)
+    
+    def set(self):
+        pass
     
 
 
